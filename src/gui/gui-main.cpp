1c8104ed869a159ccf297559e732ad29d39f7b8b
NadnerbrendaN <135467483+NadnerbrendaN@users.noreply.github.com>
2026-02-23 11:41:21 -0500

    Fix spelling in copyright header

--- src/gui/gui-main.cpp
+++ src/gui/gui-main.cpp
@@ -4,7 +4,7 @@
  * The main function that runs window.hpp and window.cpp stuff
  *
  *  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- *  If you did not recieve a copy of the MPL-2.0 with this source code, please contact the author
+ *  If you did not receive a copy of the MPL-2.0 with this source code, please contact the author
  *  to report the issue, and visit https://www.mozilla.org/media/MPL/2.0/index.f75d2927d3c1.txt
  *  to obtain a copy of the license text.
  */
